c8a432a1626b9fc42a9b453ab20e58bb5853a333
priyasood1505 <34883838+priyasood1505@users.noreply.github.com>
2018-01-18 12:40:49 +0530

    Update establish_connection.sh
    my connection string replaced

--- Thresholding/Data input methods/establish_connection.sh
+++ Thresholding/Data input methods/establish_connection.sh
@@ -2,7 +2,10 @@
 
 #  establish_connection.sh
 #  
-#
-#  Created by Asheesh k agarwal on 16/12/17.
+
 #  
-node Temperature\ sensor/establish_connection_temperature.js "HostName=Ignitedevices.azure-devices.net;DeviceId=temperaturesensor;SharedAccessKey=YXGs3doK4s1lyuwfk8F912bCJrqaUPkQX6RSMdtm/kA=" & node Vibration\ sensor/establish_connection_vibration.js "HostName=Ignitedevices.azure-devices.net;DeviceId=vibrationsensor;SharedAccessKey=JTHvEav/XjmRh/qkK6oMspKx0UX+1OUBkbgSfRAkK48=" & node Electricity\ current\ sensor/establish_connection_current.js "HostName=Ignitedevices.azure-devices.net;DeviceId=currentsensor;SharedAccessKey=JUk5DSRHZjH+ukX2eFmgw+qJGx0EA9vg1kdtB458oF8="
+#node Temperature\ sensor/establish_connection_temperature.js "HostName=Ignitedevices.azure-devices.net;DeviceId=temperaturesensor;SharedAccessKey=YXGs3doK4s1lyuwfk8F912bCJrqaUPkQX6RSMdtm/kA=" & node Vibration\ sensor/establish_connection_vibration.js "HostName=Ignitedevices.azure-devices.net;DeviceId=vibrationsensor;SharedAccessKey=JTHvEav/XjmRh/qkK6oMspKx0UX+1OUBkbgSfRAkK48=" & node Electricity\ current\ sensor/establish_connection_current.js "HostName=Ignitedevices.azure-devices.net;DeviceId=currentsensor;SharedAccessKey=JUk5DSRHZjH+ukX2eFmgw+qJGx0EA9vg1kdtB458oF8="
+
+node Temperature\ sensor/establish_connection_temperature.js "HostName=IgniteDemosIotHub.azure-devices.net;DeviceId=temperaturesensor;SharedAccessKey=uspH/YuPbri/l9Q2Bv23G0CQQo2E+D3Ld+cwCg7rQoo=" & node Vibration\ sensor/establish_connection_vibration.js "HostName=IgniteDemosIotHub.azure-devices.net;DeviceId=vibrationsensor;SharedAccessKey=BmWqIRcKdEmZPpXPoAa1rJQa0CozaHE4NFWQlKLsN20=" & node Electricity\ current\ sensor/establish_connection_current.js "HostName=IgniteDemosIotHub.azure-devices.net;DeviceId=currentsensor;SharedAccessKey=4pApZqt2qYoD7nHnZ2J4sFjx9klMb7va4oK3STnf23w="
+
+
